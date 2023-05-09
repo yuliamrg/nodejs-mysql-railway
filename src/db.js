3006408dfe0aa2b2ea5831a9c5b845cdac3d66ca
Yuliam-latop <yuliam.rivera@outlook.com>
2023-05-08 22:49:13 -0500

    🩹 Devolviendo la funcion asincrona de createPool

--- src/db.js
+++ src/db.js
@@ -1,10 +1,14 @@
-import { createPool } from 'mysql2/promise.js';
+import { createConnection } from 'mysql2/promise';
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './config.js';
 
-export const pool = createPool({
-  user: DB_USER,
-  password: DB_PASSWORD,
-  host: DB_HOST,
-  port: DB_PORT,
-  database: DB_NAME,
-});
+export const getConnectionDb = async () => {
+  const connection = await createConnection({
+    user: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: DB_PORT,
+    database: DB_NAME,
+  });
+
+  return connection;
+};
