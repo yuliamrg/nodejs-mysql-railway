00c7abd2fd57ef07a3dfb1588fba4c34502ace75
Yuliam-latop <yuliam.rivera@outlook.com>
2023-05-08 20:37:15 -0500

    🚧 Actualizando
    actualizando la configuracion de la base de datos

--- db/db.sql
+++ db/db.sql
@@ -4,9 +4,3 @@ CREATE TABLE users (
   name VARCHAR(255) NOT NULL,
   PRIMARY KEY(id)
 ) DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
-
-show tables;
-
-SELECT * FROM users;
-
-TRUNCATE TABLE users;
--- src/db.js
+++ src/db.js
@@ -1,12 +1,13 @@
 import { createConnection } from 'mysql2/promise';
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './config';
 
 export const getConnectionDb = async () => {
   const connection = await createConnection({
-    user: 'mroot',
-    password: 'toor',
-    host: 'localhost',
-    port: 3306,
-    database: 'usersdb',
+    user: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: DB_PORT,
+    database: DB_NAME,
   });
 
   return connection;
