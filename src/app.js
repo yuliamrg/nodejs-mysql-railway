afbd6838e0fc036b5e2add834e76144e35fdb267
Yuliam-latop <yuliam.rivera@outlook.com>
2023-05-08 21:11:37 -0500

    🏗️ Cambiando promesas
    Cambiando la funcion createconnection por createPool

--- src/app.js
+++ src/app.js
@@ -1,21 +1,19 @@
 import express from 'express';
-import { getConnectionDb } from './db.js';
+import { pool } from './db.js';
 import { PORT } from './config.js';
-
-const connection = await getConnectionDb();
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-  const [result] = await connection.query('SELECT * FROM users')
-  res.json(result)
+  const [result] = await pool.query('SELECT * FROM users');
+  res.json(result);
 });
 
 app.get('/ping', async (req, res) => {
   try {
-    const [result] = await connection.query('SELECT "hello world" as RESULT');
+    const [result] = await pool.query('SELECT "hello world" as RESULT');
     console.log(result);
     res.json(result);
   } catch (error) {
@@ -24,10 +22,10 @@ app.get('/ping', async (req, res) => {
 });
 
 app.get('/create', async (req, res) => {
-  const result = await connection.query('INSERT INTO users(name) VALUES ("yuliam")')
+  const result = await pool.query('INSERT INTO users(name) VALUES ("yuliam")');
   console.log(result);
   res.json(result[0]);
-})
+});
 
 app.listen(PORT);
 console.log(`server on port ${PORT}`);
--- src/db.js
+++ src/db.js
@@ -1,14 +1,10 @@
-import { createConnection } from 'mysql2/promise';
+import { createPool } from 'mysql2/promise.js';
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './config.js';
 
-export const getConnectionDb = async () => {
-  const connection = await createConnection({
-    user: DB_USER,
-    password: DB_PASSWORD,
-    host: DB_HOST,
-    port: DB_PORT,
-    database: DB_NAME,
-  });
-
-  return connection;
-};
+export const pool = createPool({
+  user: DB_USER,
+  password: DB_PASSWORD,
+  host: DB_HOST,
+  port: DB_PORT,
+  database: DB_NAME,
+});
