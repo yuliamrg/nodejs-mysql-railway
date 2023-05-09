be0d7efa8dc6835adb08aa8c8f46aa2e46219908
Yuliam-latop <yuliam.rivera@outlook.com>
2023-05-08 22:58:46 -0500

    🩹 Agragando es6
    cambiando el objeto connection por destructuracion y sacar la funcion directamente

--- src/app.js
+++ src/app.js
@@ -2,20 +2,20 @@ import express from 'express';
 import { getConnectionDb } from './db.js';
 import { PORT } from './config.js';
 
-const connection = await getConnectionDb();
+const { query } = await getConnectionDb();
 
 const app = express();
 
 app.use(express.json());
 
 app.get('/', async (req, res) => {
-  const [result] = await connection.query('SELECT * FROM users')
+  const [result] = await query('SELECT * FROM users')
   res.json(result)
 });
 
 app.get('/ping', async (req, res) => {
   try {
-    const [result] = await connection.query('SELECT "hello world" as RESULT');
+    const [result] = await query('SELECT "hello world" as RESULT');
     console.log(result);
     res.json(result);
   } catch (error) {
@@ -24,7 +24,7 @@ app.get('/ping', async (req, res) => {
 });
 
 app.get('/create', async (req, res) => {
-  const result = await connection.query('INSERT INTO users(name) VALUES ("yuliam")')
+  const result = await query('INSERT INTO users(name) VALUES ("yuliam")')
   console.log(result);
   res.json(result[0]);
 })
